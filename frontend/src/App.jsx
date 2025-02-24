a84636e72cdeb880a371ca07090b489dd5864b10
Jalen Mann <mann135@purdue.edu>
2025-02-24 11:36:51 -0500

    Separate frontend from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,25 @@ function handleClick() {
   alert('Button clicked!')
 }
 
-
+function taskbar() {
+  return (
+    <div className='taskbar'>
+      <nav className='taskbar-elem'>
+        <h3 onClick={handleClick} className='elem'>Groups</h3>
+        <h3 onClick={handleClick} className='elem'>Events</h3>
+        <h3 onClick={handleClick} className='elem'>Forum</h3>
+        <h3 onClick={handleClick} className='elem'>Messages</h3>
+        <img onClick={handleClick} className='profile' src={tempProfile} alt="" />
+      </nav>
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <>
-      <div className='taskbar'>
-        <nav className='taskbar-elem'>
-          <h3 onClick={handleClick} className='elem'>Groups</h3>
-          <h3 onClick={handleClick} className='elem'>Events</h3>
-          <h3 onClick={handleClick} className='elem'>Forum</h3>
-          <h3 onClick={handleClick} className='elem'>Messages</h3>
-          <img onClick={handleClick} className='profile' src={tempProfile} alt="" />
-        </nav>
-
-        
-      </div>
+      {taskbar()}
     </>
   )
 }
