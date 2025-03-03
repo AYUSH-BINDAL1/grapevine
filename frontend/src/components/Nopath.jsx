cc725aff4562b4448c867f3b7ae7359636638167
Jalen Mann <mann135@purdue.edu>
2025-03-02 21:01:15 -0500

    fix taskbar, add time availability, and not found page

---
+++ frontend/src/components/Nopath.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nopath.css';
+
+const Nopath = () => {
+    return (
+        <div className="nopath-container">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>The page you are looking for doesn't exist or has been moved.</p>
+            <Link to="/home" className="home-button">
+                Return Home
+            </Link>
+        </div>
+    );
+};
+
+export default Nopath;
