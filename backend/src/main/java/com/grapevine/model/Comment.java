dccf2e8f81ca095513c21b4a1e72e3d7f4931380
Zhaorong Tu <zhaorongtuzt7@gmail.com>
2025-04-15 15:35:06 -0400

    Story 8.4: Create algorithm to display a list of discussion threads

---
+++ backend/src/main/java/com/grapevine/model/Comment.java
@@ -0,0 +1,44 @@
+package com.grapevine.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "author_email", nullable = false)
+    private String authorEmail;
+
+    @Column(name = "author_name", nullable = false)
+    private String authorName;
+
+    @Column(name = "likes")
+    private Integer likes = 0;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "thread_id")
+    private Thread thread;
+}
